a9fb831d27ece90089b2c4d835e162788a908d7e
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-03-12 10:54:32 +0100

    chore: fix release script

--- newRelease.sh
+++ newRelease.sh
@@ -6,13 +6,13 @@
 # - replace cmd/version via sed
 # 
 FULL_TAG=$(svu $@)
-NO_PREFIX_TAG=$(svu $@ --strip-prefix)
+NO_PREFIX_TAG=$(svu $@ --strip-prefix | head -1)
 echo $FULL_TAG
 echo $NO_PREFIX_TAG
 
 # updating the version in graph/version.go
 go test ./... && \
-goreleaser --snapshot --skip-publish --rm-dist && \
+goreleaser --snapshot --skip-publish --clean && \
 sed -i -E "s/(Version\W+=\W*)\"(.*?)\"/\1\"$NO_PREFIX_TAG\"/" graph/version.go && \
 git add graph/version.go && git commit -m "chore: Release $FULL_TAG" && \
 git tag -a $FULL_TAG -m "Release $FULL_TAG" && \
