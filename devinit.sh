f7e4690a02c108f07ac236210951aed45d612c69
mpapenbr <Markus.Papenbrock+github@gmail.com>
2024-11-16 09:20:32 +0100

    Update environment for Go 1.23 Fixes #125

--- devinit.sh
+++ devinit.sh
@@ -1,4 +1,3 @@
-go install github.com/spf13/cobra-cli@latest
 go install github.com/goreleaser/goreleaser@latest
 go install github.com/caarlos0/svu@latest
 go install github.com/99designs/gqlgen@latest
